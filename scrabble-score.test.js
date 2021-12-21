c2dbe416c17b5dc53a2293da91a8cb410359f1d8
atemp678 <ajt2158@columbia.edu>
2021-12-21 16:51:31 +0000

    task 1 finished

--- scrabble-score.test.js
+++ scrabble-score.test.js
@@ -1,10 +1,46 @@
 import scrabbleScore from "./scrabble-score";
+//import { scrabbleTotal } from "./scrabble-score";
+
+const letterScores = [
+  ["a", 1],
+  ["b", 3],
+  ["c", 3],
+  ["d", 2],
+  ["e", 1],
+  ["f", 4],
+  ["g", 2],
+  ["h", 4],
+  ["i", 1],
+  ["j", 8],
+  ["k", 5],
+  ["l", 1],
+  ["m", 3],
+  ["n", 1],
+  ["o", 1],
+  ["p", 3],
+  ["q", 10],
+  ["r", 1],
+  ["s", 1],
+  ["t", 1],
+  ["u", 1],
+  ["v", 4],
+  ["w", 4],
+  ["x", 8],
+  ["y", 4],
+  ["z", 10],
+];
 
 // List of matchers (expects) for jest:
 // https://jestjs.io/docs/using-matchers
 
-describe("scrabble score", () => {
-  it("should score 'a' correctly", () => {
-    expect(scrabbleScore("a")).toBe(1);
+describe("scrabble score", (letter, score) => {
+  it.each(letterScores)("should score %s correctly as %i", (letter, score) => {
+    expect(scrabbleScore(letter)).toBe(score);
   });
 });
+
+// describe("scrabble total", (word, scoreTotal) => {
+//   it.each(letterScores)("Should score %s correct as %i", (word, scoreTotal) => {
+//     expect(scrabbleTotal(word)).toBe(scoreTotal);
+//   });
+// });
