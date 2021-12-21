0a733de81d5fbca0892d17b18a0fc323de74291e
Jul-Me <memai.julian@gmail.com>
2021-12-21 21:31:21 +0000

    step 1 completed

--- scrabble-score.js
+++ scrabble-score.js
@@ -1,3 +1,23 @@
-export default function (word) {
-  return 1;
+export default function (letter) {
+  if (['a', 'e', 'i', 'o', 'u', 'l', 'n', 'r', 's', 't'].includes(letter)) {
+    return 1;
+  }
+  if (['d', 'g'].includes(letter)) {
+    return 2;
+  }
+  if (['b', 'c', 'm', 'p'].includes(letter)) {
+    return 3;
+  }
+  if (['f', 'h', 'v', 'w', 'y'].includes(letter)) {
+    return 4;
+  }
+  if (['k'].includes(letter)) {
+    return 5;
+  }
+  if (['j', 'x'].includes(letter)) {
+    return 8;
+  }
+  if (['q', 'z'].includes(letter)) {
+    return 10;
+  }
 }
--- scrabble-score.test.js
+++ scrabble-score.test.js
@@ -1,10 +1,20 @@
-import scrabbleScore from "./scrabble-score";
+import scrabbleScore from './scrabble-score';
 
 // List of matchers (expects) for jest:
 // https://jestjs.io/docs/using-matchers
 
-describe("scrabble score", () => {
-  it("should score 'a' correctly", () => {
-    expect(scrabbleScore("a")).toBe(1);
+describe('scrabble score', () => {
+  //prettier-ignore
+  const letterScores = [
+    ["a", 1], ["b", 3], ["c", 3], ["d", 2], ["e", 1],
+    ["f", 4], ["g", 2], ["h", 4], ["i", 1], ["j", 8],
+    ["k", 5], ["l", 1], ["m", 3], ["n", 1], ["o", 1],
+    ["p", 3], ["q", 10], ["r", 1], ["s", 1], ["t", 1],
+    ["u", 1], ["v", 4], ["w", 4], ["x", 8], ["y", 4],
+    ["z", 10]
+  ];
+
+  it.each(letterScores)('should score %s as %i', (letter, score) => {
+    expect(scrabbleScore(letter)).toBe(score);
   });
 });
